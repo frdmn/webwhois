d5193c095c943a62fe3235c7c9eb4d62aa6febeb
Jonas Friedmann <j@frd.mn>
2016-04-11 00:05:49 +0200

    Remove database connect function

--- includes/routes.php
+++ includes/routes.php
@@ -7,24 +7,6 @@ $jsonObject = array(
   'status' => 'success'
 );
 
-/* Functions */
-
-/**
- * Establish MySQL database for further/later use
- * @return {PDO} connection instance
- */
-function getDatabaseConnection() {
-  try {
-    $db_username = constant('DB_USERNAME');
-    $db_password = constant('DB_PASSWORD');
-    $conn = new PDO('mysql:host='.constant('DB_HOSTNAME').';dbname='.constant('DB_NAME').'', $db_username, $db_password);
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  } catch(PDOException $e) {
-    echo 'ERROR: ' . $e->getMessage();
-  }
-  return $conn;
-}
-
 /* Routes */
 
 /**
