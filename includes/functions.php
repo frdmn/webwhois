9abca188e3f985c85940e11686512558529fdd01
Jonas Friedmann <j@frd.mn>
2016-04-10 22:12:57 +0200

    Commenting

--- includes/functions.php
+++ includes/functions.php
@@ -1,4 +1,11 @@
 <?php
+  /**
+   * Find integer index of a given key name (string)
+   * in given object
+   * @param  {Object} $object
+   * @param  {String} $needle
+   * @return {Integer/Boolean} false, if no result
+   */
   function getIntegerIndexOfObjectKey($object, $needle){
     $i = 0;
     foreach ($object as $objectkey => $obj) {
