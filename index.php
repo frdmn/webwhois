fa5bc88ca4f290b950f007270a5a8e4eaaa5d74c
Jonas Friedmann <j@frd.mn>
2016-04-11 00:22:32 +0200

    Implement header middlewear

--- index.php
+++ index.php
@@ -15,8 +15,34 @@ include('includes/functions.php');
 $templates = new League\Plates\Engine('templates');
 $templates->addFolder('partials', 'templates/partials');
 
+// Custom middleware inject some headers
+$headerMiddleware = function ($request, $response, $next) {
+  $response = $next($request, $response);
+
+  // Add default security header
+  $response = $response->withHeader('X-Frame-Options', 'SAMEORIGIN');
+  $response = $response->withHeader('X-Content-Type-Options', 'nosniff');
+  $response = $response->withHeader('X-XSS-Protection', '1; mode=block');
+  $response = $response->withHeader('Content-Security-Policy', "default-src 'none'; script-src 'self'; connect-src 'self'; img-src 'self'; style-src 'self';");
+
+  // If API request succeeded, add CORS headers and always return as JSON
+  $reqIsAPI = (bool) preg_match('|^/api|', $request->getUri()->getPath());
+  $resIsOk = $response->getStatusCode();
+
+  if ($reqIsAPI && $resIsOk === 200) {
+    $response = $response->withHeader('Content-Type', 'application/json');
+    $response = $response->withHeader('Access-Control-Allow-Methods', '*');
+    $response = $response->withHeader('Access-Control-Allow-Origin', '*');
+  }
+
+  return $response;
+};
+
 // Initialize Slim instance
 $app = new \Slim\App;
+
+// Add header middleware
+$app->add($headerMiddleware);
 
 // Set routes
 $app->get('/', 'dashboard');
