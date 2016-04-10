867bcdfdd11c5aae60b31248959f510d7f89368d
Jonas Friedmann <j@frd.mn>
2016-04-10 20:47:27 +0200

    Domain search form mockup

--- assets/js/_app.js
+++ assets/js/_app.js
@@ -1,3 +1,13 @@
 $(function() {
-  console.log('test');
+  // Insert the first domain package in the hidden input
+  var defaultPackage=$('#dropdownmenu > li').first().text();
+  $('#dropdowninput').val(defaultPackage);
+
+  // Update fake "select" when user clicks on package in dropdown menu
+  $('#dropdownmenu > li').click(function(e){
+    e.preventDefault();
+    var selected = $(this).text();
+    $('#dropdowninput').val(selected);
+    $('#dropdowndisplay').text(selected);
+  });
 });
--- templates/partials/dashboard.php
+++ templates/partials/dashboard.php
@@ -1,6 +1,34 @@
-<?php $this->layout('layout', ['title' => 'Dashboard']) ?>
+<?php
+  $this->layout('layout', ['title' => 'Dashboard']);
+  global $config;
+?>
 
-<div class="starter-template">
-  <h1>Bootstrap starter template</h1>
-  <p class="lead">Use this document as a way to quickly start any new project.<br> All you get is this text and a mostly barebones HTML document.</p>
-</div>
+<div class="row">
+  <form method="post">
+    <fieldset>
+      <!-- Form Name -->
+      <legend>Domain search</legend>
+
+      <div class="form-inputs">
+        <div class="input-group">
+          <span class="input-group-addon">www.</span>
+          <input type="text" class="form-control" id="your-domain" name="your-domain" placeholder="Your domain">
+          <span class="input-group-addon" id="dot">.</span>
+          <div class="input-group-btn">
+            <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              <span id="dropdowndisplay">de</span> <span class="caret"></span>
+            </button>
+            <ul class="dropdown-menu" id="dropdownmenu">
+              <li><a href="#">de</a></li>
+              <li><a href="#">com / net / org / biz / info</a></li>
+              <li><a href="#">New gTLDs</a></li>
+            </ul><!-- /.dropdown-menu -->
+            <input type="hidden" id="dropdowninput" name="tldpackage">
+          </div><!-- /.input-group-btn -->
+        </div><!-- /.input-group -->
+        <hr>
+      </div><!-- /.form-inputs -->
+    </fieldset>
+    <button type="submit" class="btn btn-primary">Search</button>
+  </form>
+</div><!-- /.row -->
