2d53fa770db41a1b4d2d10340f2011de93d47827
Jonas Friedmann <j@frd.mn>
2016-04-11 17:52:40 +0200

    Update indentation

--- includes/functions.php
+++ includes/functions.php
@@ -1,18 +1,19 @@
 <?php
-  /**
-   * Find integer index of a given key name (string)
-   * in given object
-   * @param  {Object} $object
-   * @param  {String} $needle
-   * @return {Integer/Boolean} false, if no result
-   */
-  function getIntegerIndexOfObjectKey($object, $needle){
-    $i = 0;
-    foreach ($object as $objectkey => $obj) {
-      if ($objectkey === $needle) {
-        return $i;
-      }
-      $i++;
+/**
+ * Find integer index of a given key name (string)
+ * in given object
+ * @param  {Object} $object
+ * @param  {String} $needle
+ * @return {Integer/Boolean} false, if no result
+ */
+function getIntegerIndexOfObjectKey($object, $needle){
+  $i = 0;
+  foreach ($object as $objectkey => $obj) {
+    if ($objectkey === $needle) {
+      return $i;
     }
-    return false;
+    $i++;
   }
+  return false;
+}
+
