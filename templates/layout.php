873109bab4d9d35ad9486393843aab84f3b9fea9
Jonas Friedmann <j@frd.mn>
2016-04-16 15:56:17 +0200

    Add description config

--- templates/layout.php
+++ templates/layout.php
@@ -7,7 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <meta name="description" content="iWelt Whois">
+    <meta name="description" content="<?= $config->general->description ?>">
     <meta name="author" content="Jonas Friedmann <j@frd.mn>">
 
     <title><?= $config->general->title ?> - <?= $this->e($title) ?></title>
