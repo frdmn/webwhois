a2b54fb87770ebc3ef1ce4b479a21044338416fc
Jonas Friedmann <j@frd.mn>
2016-04-25 22:59:57 +0200

    Remove unused JS code

--- public/javascripts/_app.js
+++ public/javascripts/_app.js
@@ -1,40 +1,5 @@
 $(function() {
   /**
-   * Load configuration file asynchronously
-   * @return {Callback}
-   */
-  var loadConfigFile = function(callback){
-    $.getJSON('config.json', function(data) {
-      callback(data);
-    });
-  };
-
-  /**
-   * Return all TLDs for the configured
-   * 'default-selection'
-   * @param  {Object} config
-   * @return {String} TLD(s)
-   */
-  var getDefaultSelectionTlds = function(config){
-    var defaultSelection = config.general['default-selection'];
-
-    // Search in TLD packages
-    for(var package in config.tldpackages){
-      if (package === defaultSelection) {
-        return config.tldpackages[package].tlds.join(', ');
-      }
-    }
-
-    // Search in TLDs
-    if (config.tlds.indexOf(defaultSelection) !== -1) {
-      var index = config.tlds.indexOf(defaultSelection);
-      return config.tlds[index];
-    }
-
-    return false;
-  }
-
-  /**
    * Toggle the TLD/package form (dropdown)
    * @param  {String} state "hide" or "show"
    * @return {Boolean}
@@ -156,16 +121,11 @@ $(function() {
     }
   }
 
-  // Make sure to load config file
-  loadConfigFile(function(config){
-    // Insert the first domain package in the hidden input
-    var defaultTlds = $('#dropdownmenu > li').first().data('tlds');
-    $('#tlds').val(getDefaultSelectionTlds(config));
-    $('#tld-display').text(config.general['default-selection']);
+  // Insert the first domain package in the hidden input
+  var defaultTlds = $('#dropdownmenu > li').first().data('tlds');
 
-    // Hide loading spinner on page load
-    toggleResultsTable('hide');
-  });
+  // Hide loading spinner on page load
+  toggleResultsTable('hide');
 
   // Request lookup, when single form is active
   $('.row').on('click', 'form.single .submit', function(e){
