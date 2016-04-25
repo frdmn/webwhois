68aa14ffeb46125b22657f16d38d798478ba5e69
Jonas Friedmann <j@frd.mn>
2016-04-25 12:12:50 +0200

    Pass configuration file to views

--- app.js
+++ app.js
@@ -2,8 +2,11 @@ var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
+var fs = require('fs');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+
+var configuration = require('./config.json')
 
 var routeIndex = require('./routes/index');
 var routeApi = require('./routes/api');
@@ -14,13 +17,14 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+configuration.version = fs.readFileSync('./VERSION', 'utf8');
+app.locals.configuration = configuration;
 
 app.use('/', routeIndex);
 app.use('/users', routeApi);
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {
+    title: 'Dashboard',
+    configuration: req.app.locals.configuration
+  });
 });
 
 module.exports = router;
